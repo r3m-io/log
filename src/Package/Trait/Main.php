227c175543f940dacc11621982e528c2a5e2e31c
remco van der Velde <remco.pc@outlook.com>
2024-03-15 18:08:49 +0100

    log clear

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -15,6 +15,27 @@ trait Main {
             throw new Exception('Option channel is required...');
         }
         $object = $this->object();
+        if(!is_array($options->channel)){
+            $options->channel = [$options->channel];
+        }
+        foreach($options->channel as $channel){
+            $log = $object->config('log.' . $channel);
+            if($log){
+                $log = new Data($log);
+                $handler = $log->get('handler');
+                if(is_array($handler)){
+                    foreach($handler as $node){
+                        $node = new Data($node);
+                        $parameters = $node->get('parameters');
+                        if($parameters){
+                            $parameters = new Data($parameters);
+                            $parameters = Config::parameters($object, $parameters);
+                            ddd($parameters);
+                        }
+                    }
+                }
+            }
+        }
         $log = $object->config('log.' . $options->channel);
         if($log){
             $log = new Data($log);
