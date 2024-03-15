e7c68b1af5fbfb30326cc1a9aa42c75895e667ab
remco van der Velde <remco.pc@outlook.com>
2024-03-15 09:21:35 +0100

    log clear

--- src/Package/Trait/Import.php
+++ src/Package/Trait/Import.php
@@ -65,8 +65,7 @@ trait Import {
             $options->uuid = true;
             $node = new Node($object);
             $response = $node->import($class, $node->role_system(), $options);
-            d($response);
-            $node->stats($class, $response);
+//            $node->stats($class, $response);
         }
     }
 
